e24603bb4f4547c5bf24b9a19de7e80c2ca75e66
ChillTeensie <stasieniekwu@gmail.com>
2021-07-31 11:22:55 +0200

    update some crap

---
+++ source/CreditsSubState.hx
@@ -0,0 +1,71 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+
+class CreditsSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+	public static var currChanges:String = "dk";
+	
+	private var bgColors:Array<String> = [
+		'#314d7f',
+		'#4e7093',
+		'#70526e',
+		'#594465'
+	];
+	private var colorRotation:Int = 1;
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('week54prototype', 'shared'));
+		//bg.scale.x *= 1.55;
+		//bg.scale.y
+		bg.screenCenter();
+		
+		var kadeLogo:FlxSprite = new FlxSprite(FlxG.width, 0).loadGraphic(Paths.image('KadeEngineLogo'));
+		kadeLogo.scale.y = 0.3;
+		kadeLogo.scale.x = 0.3;
+		kadeLogo.x -= kadeLogo.frameHeight;
+		kadeLogo.y -= 180;
+		kadeLogo.alpha = 0.8;
+		
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"hey, this demo is still a wip!\n"
+			+ "everything you see in this demo is subject to change."
+			+ "\nPress space to countinue.",
+			32);
+		
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.fromRGB(200, 200, 200), CENTER);
+		txt.borderColor = FlxColor.BLACK;
+		txt.borderSize = 3;
+		txt.borderStyle = FlxTextBorderStyle.OUTLINE;
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
